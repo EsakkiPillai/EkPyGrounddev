a5753fd4e0f036b7dd6764e9bb9cdf09086fc69c
EsakkiPillai <esakkisankart@gmail.com>
2018-02-26 18:29:05 +0530

    My First Python Prg Commit

---
+++ helloWorld.py
@@ -0,0 +1,24 @@
+# Chapter 1 - Simple Examples
+print("Hello World Program in Python")
+
+print("Welcome to Programming")
+
+"""
+getting  Input From the user
+For multiLine Comment use CTRL + /
+
+"""
+
+name=input("Please Enter Your Name:\t")
+
+print("Welcome to the Python World :"+ name+"!!!!")
+
+"""
+Black slash Character used to escape 
+"""
+
+age=26
+print(name+" is " + age + " old ")
+
+# division operator will return float to get intefer then we have to use //
+
